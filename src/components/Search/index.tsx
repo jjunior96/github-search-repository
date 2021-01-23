0108dfa059cfb7d74e6d0f9ac43a1d2b47db4846
Junior Alves <jjalves96@gmail.com>
2021-01-23 08:16:06 -0300

    feat: create Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+const Search: React.FC = () => {
+  return (
+    <S.SearchWrapper>
+      <h1>Teste</h1>
+    </S.SearchWrapper>
+  )
+}
+
+export default Search;
