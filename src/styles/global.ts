8364cfbaeefe74858bcc1aa45256dc8b6d4f4dfb
Junior Alves <jjalves96@gmail.com>
2021-01-27 08:50:00 -0300

    chore: add new styles into project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,8 +7,16 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
+  html {
+    font-size: 62.5%;
+  }
 
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
   button {
