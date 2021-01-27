552a9664d6f45086c79bbc4180d33c3afbe61453
Junior Alves <jjalves96@gmail.com>
2021-01-27 08:49:17 -0300

    feat: create Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { InputWrapper } from './styles';
+
+const Input: React.FC = () => {
+  return (
+    <InputWrapper placeholder="teste" />
+  );
+};
+
+export default Input;
