76c70d408c5660da0358b3cbc4d58da3123327de
Junior Alves <jjalves96@gmail.com>
2021-01-27 08:49:36 -0300

    feat: create dashboard page

---
+++ src/pages/dashboard/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #F0F0F2;
+`;
+
+export const Title = styled.h1`
+  font-size: 4.8rem;
+  color: #2F3640;
+`;
